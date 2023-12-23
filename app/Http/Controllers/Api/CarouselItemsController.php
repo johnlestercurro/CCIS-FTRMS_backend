fb7e5675d5497b056aec6fcff5d9ff8e4e5243ba
Alrie-Craig222 <almariemaestrado05@gmail.com>
2023-12-23 09:29:31 +0800

    Added Show Functionality in CarouselItemsController

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -37,7 +37,7 @@ class CarouselItemsController extends Controller
      */
     public function show(string $id)
     {
-        //
+        return CarouselItems::findOrfail($id);	
     }
 
     /**
